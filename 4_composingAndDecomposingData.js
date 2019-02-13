ecc35d91e9d62b948bf2f863e590713c6f957309
Matthew McGrath <matthew.mcgrath12@gmail.com>
2019-02-12 22:35:03 -0500

    how to flatten. recursive thinking

--- 4_composingAndDecomposingData.js
+++ 4_composingAndDecomposingData.js
@@ -39,3 +39,71 @@ console.log(first, butFirst);
 const numbers = (...nums) => nums;
 
 console.log(numbers(1, 2, 3, 4, 5));
+
+// Self-similarity
+
+// A list is:
+// - Empty; or
+// - Consists of an element concatenated with a list
+
+const list1 = [];
+const list2 = ['baz', ...[]];
+const list3 = ['foo', ...['bar', 'baz']];
+
+console.log(list3);
+
+const [firstOne, ...rest] = ['foo', 'bar', 'baz'];
+
+console.log(firstOne);
+console.log(...rest);
+
+// finding the length of an array, in a cool way
+
+const lengthOfArray = ([first, ...rest]) =>
+    first === undefined ?
+    0 :
+    // @ts-ignore
+    1 + lengthOfArray(rest);
+
+console.log(lengthOfArray([1, 2, 3]))
+console.log(lengthOfArray([1]));
+
+// recursion party trick.
+
+// recursion problem solving
+// - 1 - divide the problem into smaller problems
+// - 2 - if the smaller problem is solvable, solve
+// - 3 - if the smaller problem is not solvable, divide and conquer that problem
+// - 4 - when all small problems have been solved, compose into one big solution
+
+// term = linear recursion
+
+// test for the terminal case
+
+// example: flatten an array - turn array of arrays into one array of elements that are not arrays
+// problems to solve:
+// - is something an array
+// - extract element from array
+// - add element to new array
+// - go through array in order
+
+// terminal case:
+// - do nothing with empty array
+// - if element is not an array, add to new array
+// - if element is an array, flatten by checking the inner arrays elements
+
+const flatten = ([first, ...rest]) => {
+    if (first === undefined) {
+        return [];
+    } else if (!Array.isArray(first)) {
+        // @ts-ignore
+        return [first, ...flatten(rest)];
+    } else {
+        // @ts-ignore
+        return [...flatten(first), ...flatten(rest)]
+    }
+}
+
+console.log(flatten(['foo', [1, 2, 'bar'],
+    ['3', 5]
+]));
